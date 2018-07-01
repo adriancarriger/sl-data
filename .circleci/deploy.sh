317b5e25b3afc03a85b09033d2f9a732d6bac288
Adrian Carriger <hello@adriancarriger.com>
2018-07-01 17:39:40 -0500

    chore(deploy): add env specific secrets

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,9 +8,13 @@ cd "$( dirname "$0" )" && cd ..
 prisma login -k $PRISMA_CLI_KEY &>/dev/null
 
 if [[ $CIRCLE_BRANCH == "develop" ]]; then
-  PRISMA_ENDPOINT=https://us1.prisma.sh/adrian-carriger-35d3e6/sl-data/staging prisma deploy
+  PRISMA_ENDPOINT=https://us1.prisma.sh/adrian-carriger-35d3e6/sl-data/staging \
+  PRISMA_SECRET=PRISMA_SECRET_STAGING \
+  prisma deploy
 fi
 
 if [[ $CIRCLE_BRANCH == "master" ]]; then
-  PRISMA_ENDPOINT=https://us1.prisma.sh/adrian-carriger-35d3e6/sl-data/production prisma deploy
+  PRISMA_ENDPOINT=https://us1.prisma.sh/adrian-carriger-35d3e6/sl-data/production \
+  PRISMA_SECRET=PRISMA_SECRET_PRODUCTION \
+  prisma deploy
 fi
