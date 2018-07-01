c824928780e788d22626611417f1b692c08a2745
Adrian Carriger <hello@adriancarriger.com>
2018-07-01 15:36:10 -0500

    fix(ci): hide secrets

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 # Run from parent directory
 cd "$( dirname "$0" )" && cd ..
 
-prisma login -k $PRISMA_CLI_KEY
+prisma login -k $PRISMA_CLI_KEY &>/dev/null
 
 if [[ $CIRCLE_BRANCH == "develop" ]]; then
   PRISMA_ENDPOINT=https://us1.prisma.sh/adrian-carriger-35d3e6/sl-data/staging prisma deploy
