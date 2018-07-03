02b3fb615ad49be9b142e376beb37a6ca01a40c1
Adrian Carriger <hello@adriancarriger.com>
2018-07-03 14:34:44 -0500

    fix(deploy): correctly assign secrets

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -9,12 +9,12 @@ prisma login -k $PRISMA_CLI_KEY &>/dev/null
 
 if [[ $CIRCLE_BRANCH == "develop" ]]; then
   PRISMA_ENDPOINT=https://us1.prisma.sh/adrian-carriger-35d3e6/sl-data/staging \
-  PRISMA_SECRET=PRISMA_SECRET_STAGING \
+  PRISMA_SECRET=$PRISMA_SECRET_STAGING \
   prisma deploy
 fi
 
 if [[ $CIRCLE_BRANCH == "master" ]]; then
   PRISMA_ENDPOINT=https://us1.prisma.sh/adrian-carriger-35d3e6/sl-data/production \
-  PRISMA_SECRET=PRISMA_SECRET_PRODUCTION \
+  PRISMA_SECRET=$PRISMA_SECRET_PRODUCTION \
   prisma deploy
 fi
